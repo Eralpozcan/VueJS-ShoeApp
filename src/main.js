545130d4f3b18811eb562a12d2abdb6c3fc4fe4f
Eralpozcan <eralpozcans@gmail.com>
2024-01-07 04:03:48 +0300

    fix: typo

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ Sentry.init({
     }),
   ],
   // Performance Monitoring
-  tracesSampleRate: 0.3 //  Capture 100% of the transactions
+  tracesSampleRate: 0.3, //  Capture 100% of the transactions
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 0.9, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
